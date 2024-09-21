c19d3af696af113caa8b3805659dc085a62884a9
Nestor1760 <romannesterchuk01@gmail.com>
2024-09-21 20:43:09 +0200

    add: zod for validation inputs

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,12 +1,33 @@
 import { db } from "@/app/lib/db";
 import { NextResponse } from "next/server";
 import { hash } from 'bcrypt'
+import * as z from 'zod'
+
+//Define a schema for input validation
+const userSchema = z.object({
+  username: z
+    .string()
+    .min(1, "User is required")
+    .max(50),
+  email: z
+    .string()
+    .min(1, "Email is required")
+    .email('Invalid email'),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(8, "Password must have than 8 characters"),
+})
 
 export async function POST(req: Request) {
   try {
     const body = await req.json()
 
-    const { email, username, password } = body
+    const {
+      email,
+      username,
+      password
+    } = userSchema.parse(body)
 
     //check if email already exists
     const existingUserByEmail = await db.user.findUnique({
@@ -14,7 +35,10 @@ export async function POST(req: Request) {
     });
 
     if (existingUserByEmail) {
-      return NextResponse.json({ user: null, message: "User with this email already exists" }, { status: 409 })
+      return NextResponse.json(
+        { user: null, message: "User with this email already exists" },
+        { status: 409 }
+      )
     }
 
     //check if username already exists
@@ -23,7 +47,10 @@ export async function POST(req: Request) {
     });
 
     if (existingUserByUsername) {
-      return NextResponse.json({ user: null, message: "User with this username already exists" }, { status: 409 })
+      return NextResponse.json(
+        { user: null, message: "User with this username already exists" },
+        { status: 409 }
+      )
     }
 
     const hashedPassword = await hash(password, 10)
@@ -36,10 +63,19 @@ export async function POST(req: Request) {
       }
     })
 
-    const { password: newUserPassword, ...userInfo } = newUser
+    const {
+      password: newUserPassword,
+      ...userInfo
+    } = newUser
 
-    return NextResponse.json({ user: userInfo, message: "User created successufully" }, { status: 201 })
+    return NextResponse.json(
+      { user: userInfo, message: "User created successufully" },
+      { status: 201 }
+    )
   } catch (error) {
-    return NextResponse.json({ message: "Something went wrong!" }, { status: 500 })
+    return NextResponse.json(
+      { message: "Something went wrong!" },
+      { status: 500 }
+    )
   }
 }
